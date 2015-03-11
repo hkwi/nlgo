7bc5826c45192fc35841c1a27699f554443ef8f4
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2015-03-11 18:34:52 +0900

    add stat

--- rt.go
+++ rt.go
@@ -74,6 +74,35 @@ const (
 	IFLA_PORT_RESPONSE
 )
 
+type RtnlLinkStats64 struct {
+	RxPackets uint64
+	TxPackets uint64
+	RxBytes uint64
+	TxBytes uint64
+	RxErrors uint64
+	TxErrors uint64
+	RxDropped uint64
+	TxDropped uint64
+	Multicast uint64
+	Collisions uint64
+	// detailed rx_errors
+	RxLengthErrors uint64
+	RxOverErrors uint64
+	RxCrcErrors uint64
+	RxFrameErrors uint64
+	RxFifoErrors uint64
+	RxMissedErrors uint64
+	// detailed tx_errors
+	TxAbortedErrors uint64
+	TxCarrierErrors uint64
+	TxFifoErrors uint64
+	TxHeartbeatErrors uint64
+	TxWindowErrors uint64
+	// cslip etc.
+	RxCompressed uint64
+	TxCompressed uint64
+}
+
 var portPolicy Policy = MapPolicy{
 	Prefix: "IFLA_PORT",
 	Names:  IFLA_PORT_itoa,
